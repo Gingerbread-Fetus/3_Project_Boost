ba32d5c439e918de1f00f3ab837c0bec0392d156
Hayden Avery Matthews <haydenamatthews@yahoo.com>
2020-03-17 15:52:06 -0600

    Added level loading.

--- 3_Project_Boos/Assets/Rocket.cs
+++ 3_Project_Boos/Assets/Rocket.cs
@@ -1,78 +1,101 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
-    Rigidbody myRigidBody;
-    AudioSource myAudioSource;
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    State state = State.Alive;
 
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 100f;
 
+    enum State
+    {
+        Alive,
+        Dying,
+        Transcending
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        myRigidBody = GetComponent<Rigidbody>();
-        myAudioSource = GetComponent<AudioSource>();
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            Thrust();
+            Rotate(); 
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if(state != State.Alive) { return; }
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 print("OK");
                 break;
-            case "Fuel":
-                print("Fuel");
+            case "Finish":
+                state = State.Transcending;
+                Invoke("LoadNextLevel", 1f);
                 break;
             default:
-                print("Dead");
+                state = State.Dying;
+                audioSource.Stop();
+                Invoke("LoadFirstLevel", 1f);
                 break;
         }
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
     }
 
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            myRigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!myAudioSource.isPlaying)
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
             {
-                myAudioSource.Play();
+                audioSource.Play();
             }
         }
         else
         {
-            myAudioSource.Stop();
+            audioSource.Stop();
         }
     }
 
     private void Rotate()
     {
-        myRigidBody.freezeRotation = true;// take manual control of rotation
+        rigidBody.freezeRotation = true;// take manual control of rotation
         float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A))
         {
-            
+
             transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward * rotationThisFrame);
-        }
+        } 
 
-        myRigidBody.freezeRotation = false; // resume physics control of rotation
+        rigidBody.freezeRotation = false; // resume physics control of rotation
     }
 
 }
